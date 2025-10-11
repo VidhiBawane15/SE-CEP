945fd80fd376e43f221df538b06a7dfc3759b104
VidhiBawane15 <bvidhi4vidhi@gmail.com>
2025-10-11 17:36:04 +0530

    FileHandling.java

--- FileHandling.java
+++ FileHandling.java
@@ -1,6 +1,8 @@
 import java.io.*;
-class FileHandling {
-  public static void main(String[] args) throws Exception {
+class FileHandling 
+{
+  public static void main(String[] args) throws Exception 
+  {
     FileWriter fw = new FileWriter("sample.txt");
     fw.write("Hello, this is a file handling demo!");
     fw.close();
