ff9fbb22bd6dc9913fa2c6eb9eef734eae881ad8
VidhiBawane15 <bvidhi4vidhi@gmail.com>
2025-10-11 17:18:10 +0530

    Create FileHandling.java

---
+++ FileHandling.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class FileHandling {
+  public static void main(String[] args) throws Exception {
+    FileWriter fw = new FileWriter("sample.txt");
+    fw.write("Hello, this is a file handling demo!");
+    fw.close();
+    BufferedReader br = new BufferedReader(new FileReader("sample.txt"));
+    System.out.println("File content: " + br.readLine());
+    br.close();
+  }
+}
